178efe2f8af246480d37824cd4569a3e42707dc8
Alvin Daeli <alvindaeli748@gmail.com>
2021-06-06 16:52:37 +0700

    Merge data

--- application/views/user/sumbangan.php
+++ application/views/user/sumbangan.php
@@ -5,11 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-<<<<<<< HEAD
-    <link rel="stylesheet" href="assets/user_style.css" />
-=======
     <link rel="stylesheet" href="../assets/user_style.css" />
->>>>>>> 78bf5b4ea8b82d63dfc881255d905851e57fc5f5
 
 </head>
 <body>
@@ -31,45 +27,26 @@
     <form enctype="multipart/form-data" method="post">
       <label for="name" id="name-label">
         Nama Lengkap
-<<<<<<< HEAD
         <input type="text" name="nama" id="name" placeholder="tuliskan nama lengkap anda" required />
-=======
-        <input type="text" id="name" placeholder="tuliskan nama lengkap anda" required />
->>>>>>> 78bf5b4ea8b82d63dfc881255d905851e57fc5f5
       </label>
 
       <label for="email" id="email-label">
         Email
-<<<<<<< HEAD
         <input type="email" name="email" id="email" placeholder="tuliskan email anda" required />
-=======
-        <input type="email" id="email" placeholder="tuliskan email anda" required />
->>>>>>> 78bf5b4ea8b82d63dfc881255d905851e57fc5f5
       </label>
 
       <label for="sifat" id="sifat-label"> Sumbangan bersifat? </label>
 
-<<<<<<< HEAD
-      <input type="radio" name="sifat" value="1" /> Pribadi <br />
-=======
       <input type="radio" name="sifat" value="1" checked /> Pribadi <br />
->>>>>>> 78bf5b4ea8b82d63dfc881255d905851e57fc5f5
       <input type="radio" name="sifat" value="2" /> Angkatan <br /> <br>
 
       <label for="kelulusan" id="kelulusan-label">
         Tahun kelulusan
-<<<<<<< HEAD
         <input type="number" name="kelulusan" id="kelulusan" placeholder="Tahun keberapa anda lulus?" min="1" required="" />
-=======
-        <input type="number" id="kelulusan" placeholder="Tahun keberapa anda lulus?" min="1" />
->>>>>>> 78bf5b4ea8b82d63dfc881255d905851e57fc5f5
       </label>
 
       <label for="sumbangan" id="sumbangan-label">
         Apa yang ingin disumbangkan?
-<<<<<<< HEAD
-        <textarea name="sumbangan" id="sumbangan" cols="30" rows="10" required=""></textarea>
-=======
       </label>
       <input type="radio" name="sumbangan" value="1" checked /> Barang <br />
       <input type="radio" name="sumbangan" value="2" /> Uang <br /> <br>
@@ -77,7 +54,6 @@
       <label for="Keterangan_sumbangan" id="sumbangan-label">
         Keterangan Sumbangan?
         <textarea id="Keterangan_sumbangan" cols="30" rows="10"></textarea>
->>>>>>> 78bf5b4ea8b82d63dfc881255d905851e57fc5f5
       </label>
 
       <div class="submit"><input type="submit" value="Submit"></div>
@@ -88,8 +64,4 @@
         <p>&copy; Copyright kelompok 7 PI kom C</p>
        </div>
 </body>
-<<<<<<< HEAD
 </html>
-=======
-</html>
->>>>>>> 78bf5b4ea8b82d63dfc881255d905851e57fc5f5
