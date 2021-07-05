8ef2b3ec14cdc92d71620f20d1d2a74bb0423456
wind_create <nardowilli@gmail.com>
2021-07-05 13:48:32 +0700

    url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://47.254.251.165:83/Tubes_PI/';
+$config['base_url'] = 'http://47.254.251.165:83/';
 
 /*
 |--------------------------------------------------------------------------
